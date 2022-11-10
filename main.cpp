4695e351effa7cc0a428177e2a6aa6bc27fa686a
S5698826 <117889839+S5698826@users.noreply.github.com>
2022-11-10 13:53:29 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,27 @@
 #include <iostream>
 using namespace std;
+int main()
+{
+    float a=0;
+    float b=0;
+    float c=0;
 
-int main(){
-//Qui vanno definite le variabili
+    cout << "inseisci i dati per a=";
+    cin>>a;
+    cout << "inserisci i dati per b=";
+    cin>>b;
+    cout << "insrisci i dati per c=";
+    cin>>c;
 
-cin >> a >> b >> c;
+    float areatriangolo= (a*b)/2;
+    float areaquadrato= (a*a);
+    float areareattangolo= (a*b);
+    float areatrapezio= ((a+b)*c)/2;
 
-//Qui va scritto il codice per eseguire i calcoli richiesti
+    cout << "area triangolo=" << areatriangolo <<  endl;
+    cout <<"area quadrato=" << areaquadrato << endl;
+    cout  << "area rettangolo=" << areareattangolo << endl;
+    cout << "area trapezio=" << areatrapezio << endl;
 
-
+    return 0;
 }
