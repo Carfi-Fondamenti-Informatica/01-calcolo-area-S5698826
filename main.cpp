5a0aeb1a2b327ddb2562318656854c0ea83fb97f
S5698826 <117889839+S5698826@users.noreply.github.com>
2022-11-11 06:37:43 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,22 +5,19 @@ int main()
     float a=0;
     float b=0;
     float c=0;
-   
+
     cout << "inseisci i dati per a=";
     cin>>a;
     cout << "inserisci i dati per b=";
     cin>>b;
     cout << "insrisci i dati per c=";
     cin>>c;
-    
-    float areatriangolo= (a*b)/2;
-    float areaquadrato= (a*a);
-    float areareattangolo= (a*b);
-    float areatrapezio= ((a+b)*c)/2;
-    
-    cout << "area triangolo=" << areatriangolo <<  endl;
-    cout <<"area quadrato=" << areaquadrato << endl;
-    cout  << "area rettangolo=" << areareattangolo << endl;
-    cout << "area trapezio=" << areatrapezio << endl;
+
+    cout << "area triangolo=" << (a*b)/2 <<  endl;
+    cout <<"area quadrato=" << (a*a) << endl;
+    cout  << "area rettangolo=" << (a*b)<< endl;
+    cout << "area trapezio=" << ((a+b)*c)/2 << endl;
+
     return 0;
 }
+
